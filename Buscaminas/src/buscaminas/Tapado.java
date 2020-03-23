097066511b07980ee482913ce17ff2e8f9a1aad5
AdrianOrueta00 <48363332+AdrianOrueta00@users.noreply.github.com>
2020-03-23 18:27:55 +0100

    Estado y Tapado corregidos y actualizados con comentarios

--- Buscaminas/src/buscaminas/Tapado.java
+++ Buscaminas/src/buscaminas/Tapado.java
@@ -3,11 +3,13 @@ public class Tapado implements Estado{
 	public Tapado() {
 		
 	}
-	public void clickIzq(Casilla pCasilla) {
+	public int clickIzq(Casilla pCasilla) { //Devuelve la informacion de la casilla, -1 si bomba, sino n de bombas adyacentes
 		pCasilla.cambiarEstado(new Destapado());
-	}
+		return pCasilla.getInfoCasilla();
+		}
 	
-	public void clickDer(Casilla pCasilla) {
-		
+	public int clickDer(Casilla pCasilla) {//Devuelve 11, colocar banderin
+		pCasilla.cambiarEstado(new Marcado());
+		return 11;
 	}
 }
