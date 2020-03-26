98b5730af65600fdd08d500da841d20450c730fe
AdrianOrueta00 <48363332+AdrianOrueta00@users.noreply.github.com>
2020-03-26 15:02:41 +0100

    Codigo comentado y mediolegible

--- Buscaminas/src/buscaminas/Casilla.java
+++ Buscaminas/src/buscaminas/Casilla.java
@@ -17,12 +17,12 @@ public class Casilla {
 	public void cambiarEstado(Estado pEstado) {
 		this.estado=pEstado;
 	}
-	public int getInfoCasilla() {
+	public int getInfoCasilla() { //Devuelve el numero contenido en el campo infoCasila
 		return this.infoCasilla;
 	}
 	public void incrementar(){ this.infoCasilla++;}
 	
-	public boolean clear() {
+	public boolean clear() { //Devuelve true si ya esta destapada
 		return (this.estado instanceof Destapado);
 	}
 }
