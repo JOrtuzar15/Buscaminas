9a1b1e1f2d777b6467a02e5f2e8e970daa4dff42
Eneko <Eneko@DESKTOP-3OFAKQG>
2020-05-07 11:43:57 +0200

    SUBIDO A CARPETA ENEKO

--- Buscaminas/src/Eneko/PantallaRanking.java
+++ Buscaminas/src/Eneko/PantallaRanking.java
@@ -16,44 +16,59 @@ import java.awt.Font;
 import java.awt.FlowLayout;
 import java.awt.SystemColor;
 import java.awt.Color;
+import javax.swing.SwingConstants;
+import javax.swing.DropMode;
+import javax.swing.border.LineBorder;
+import java.awt.Button;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class PantallaRanking extends JFrame {
+public class PantallaRanking extends JFrame implements ActionListener {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	
+	private JButton btnAceptar;
+
+
 
 	public PantallaRanking() {
 		setTitle("RANKING");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 250, 400);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
+		contentPane.setLayout(null);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(SystemColor.activeCaptionBorder);
-		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
-		flowLayout.setAlignment(FlowLayout.LEFT);
-		contentPane.add(panel_1);
+		JLabel lblNewLabel = new JLabel("L@S DIEZ MEJORES");
+		lblNewLabel.setBounds(11, 11, 213, 24);
+		lblNewLabel.setFont(new Font("Microsoft YaHei Light", Font.BOLD, 16));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel);
 		
 		JTextPane txtpnPdatosPdatosPdatos = new JTextPane();
-		txtpnPdatosPdatosPdatos.setForeground(Color.BLUE);
-		txtpnPdatosPdatosPdatos.setBackground(SystemColor.activeCaptionBorder);
+		txtpnPdatosPdatosPdatos.setBounds(11, 46, 213, 218);
+		contentPane.add(txtpnPdatosPdatosPdatos);
+		txtpnPdatosPdatosPdatos.setForeground(Color.BLACK);
+		txtpnPdatosPdatosPdatos.setBackground(Color.WHITE);
 		txtpnPdatosPdatosPdatos.setEditable(false);
 		txtpnPdatosPdatosPdatos.setFont(new Font("Arial", Font.BOLD, 18));
-		panel_1.add(txtpnPdatosPdatosPdatos);
 		txtpnPdatosPdatosPdatos.setText(Ranking.getMiRanking().jugadorYPuntuacion());
+		
+		btnAceptar = new JButton("ACEPTAR");
+		btnAceptar.addActionListener(this);
+		btnAceptar.setBounds(10, 291, 214, 59);
+		contentPane.add(btnAceptar);
 		this.setVisible(true);
 	}
-	
-	
-	
-	
-	
-
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(btnAceptar)) {
+			this.setVisible(false);
+			
+	}
+	}
 }
